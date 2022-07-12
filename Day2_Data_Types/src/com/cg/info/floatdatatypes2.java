da11d05dd8d64568b5b9a9b35b86ed2d11fca7ed
rahul7900 <rahul.singh.7920000@gmail.com>
2022-07-12 19:17:44 +0530

    data_types

---
+++ Day2_Data_Types/src/com/cg/info/floatdatatypes2.java
@@ -0,0 +1,15 @@
+package com.cg.info;
+
+import java.util.Scanner;
+
+public class floatdatatypes2 {
+
+	public static void main(String[] args) {
+		System.out.println("Enter the float:");
+		Scanner in = new Scanner(System.in);
+		float a = in.nextFloat();
+		System.out.println(a);
+		
+	}
+
+}
---
+++ Day2_Data_Types/src/com/cg/info/intdatatype1.java
@@ -0,0 +1,15 @@
+package com.cg.info;
+
+import java.util.Scanner;
+
+public class intdatatype1 {
+
+	public static void main(String[] args) {
+		System.out.println("Enter the interger:");
+		Scanner in = new Scanner(System.in);
+		int a = in.nextInt();
+		System.out.println(a);
+		
+	}
+
+}
